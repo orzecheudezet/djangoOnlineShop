ef38740e7997a4659cf5e44418206c5ecbf07188
orzecheudezet <krzysztof.m.orzechowski@gmail.com>
2020-01-20 22:44:34 +0100

    complete

--- source/carts/urls.py
+++ source/carts/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from .views import (
 cart_home,
 cart_update,
-checkout_home
+checkout_home,
+checkout_done_view,
+
 )
 
 app_name = 'carts'
@@ -11,6 +13,8 @@ app_name = 'carts'
 urlpatterns = [
 
     path('', cart_home, name='home'),
+    path('checkout/success/', checkout_done_view, name='success'),
     path('checkout/', checkout_home, name='checkout'),
     path('update/', cart_update, name='update'),
+
 ]
